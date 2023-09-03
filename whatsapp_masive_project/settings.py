6dbae65a21bbb147d3aacb309e5f714698bcf3aa
Sebastian Felipe Sanchez Hurtado <sebaspipe2000@gmail.com>
2023-09-02 23:16:25 -0500

    Change django version and variables

--- whatsapp_masive_project/settings.py
+++ whatsapp_masive_project/settings.py
@@ -21,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$&09!e0pz2^399j^6r1net^#&penqpj29f5e2md6upgx#^-sbs'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
 
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+    
 
 # Application definition
 
